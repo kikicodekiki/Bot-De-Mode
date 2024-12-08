c036649bbef793237b4bbae7aa04808ecdf66d63
Ekaterina Stoyanova <147726778+kikicodekiki@users.noreply.github.com>
2024-12-09 00:01:35 +0200

    Update main.py
    add buttons

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot(token = '')
 
@@ -20,5 +21,16 @@ to_send = """, and welcome to my Python project for the Introduction to programm
 @bot.message_handler(commands = ['start'])
 def start(message):
     bot.send_message(message.chat.id, f"<b>Hello, dear {message.from_user.first_name}</b>" + to_send, parse_mode='html')
+    # create the inline buttons
+    markup = types.InlineKeyboardMarkup()
+    notification_button = types.InlineKeyboardButton(text='Daily Trend Updates', callback_data='trend_updates')
+    help_button = types.InlineKeyboardButton(text='Help', callback_data='help')
+    markup.row(notification_button, help_button)
+    bot.send_message(message.chat.id, 'Available commands: ', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'trend_updates':
+        bot.answer_callback_query(callback.id)
 
 bot.polling(non_stop=True)
